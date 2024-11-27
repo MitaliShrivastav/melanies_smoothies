59f8c7222a6c1644b568cddf4329d0b179626bac
Mitali Shrivastav <125380062+MitaliShrivastav@users.noreply.github.com>
2024-11-27 12:27:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,15 +33,16 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string = ''
     
-    for i in ingredients_list:
-        ingredients_string += i + ' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        st.subheader(i + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + i)
-        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice_response.com/api/fruit" + search_on)
+        fv_df = st.dataframe(data = fuityvice_response.json(), use_container_width = True)
  
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" +name_on_order+ """')"""
