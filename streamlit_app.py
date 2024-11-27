f6bc89d8f7006911849be7183d22032bec885a2c
Mitali Shrivastav <125380062+MitaliShrivastav@users.noreply.github.com>
2024-11-27 12:36:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ st.write("Name on the smoothie will be", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
@@ -40,9 +40,8 @@ if ingredients_list:
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
         fruityvice_response = requests.get("https://fruityvice_response.com/api/fruit" + search_on)
-        fv_df = st.dataframe(data = fuityvice_response.json(), use_container_width = True)
+        fv_df = st.dataframe(data=fuityvice_response.json(), use_container_width = True)
  
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" +name_on_order+ """')"""
